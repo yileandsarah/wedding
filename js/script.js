dac901afb7cf2419b7539247d786ff33d88be1a5
Yi Le and Sarah <yileandsarah@gmail.com>
2019-01-08 21:33:50 +0800

    Start of project
    Created index.html.
    Created fixed top navbar - responsive when window is resized.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Responsive Navigation Bar */
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
