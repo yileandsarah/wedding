0a5eff94e9851e48bb7165486c59f4ca1f6f46ee
Yi Le and Sarah <yileandsarah@gmail.com>
2019-01-26 23:23:53 +0800

    Failed modal attempt 2

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/* Responsive Navigation Bar */
-function myFunction() {
-  var x = document.getElementById("navbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
